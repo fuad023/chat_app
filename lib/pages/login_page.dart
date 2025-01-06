543799a0159d65e0455c14fc0875fb8d54151ab2
fuad <nafisfuadisc@gmail.com>
2025-01-06 23:30:10 +0600

    Email Authentication

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:chat_app/components/my_textfield.dart';
 import 'package:chat_app/components/my_button.dart';
+import 'package:chat_app/auth/auth_service.dart';
 
 class LoginPage extends StatelessWidget {
   // email and pw text controllers
@@ -13,7 +14,25 @@ class LoginPage extends StatelessWidget {
   LoginPage({super.key, required this.onTap});
 
   // login method
-  void login() {}
+  void login(BuildContext context) async {
+    // auth service
+    final authService = AuthService();
+
+    // try login
+    try {
+      await authService.signInWithEmailPassword(_emailController.text, _pwController.text);
+    }
+    
+    // catch any errors
+    catch (e) {
+      showDialog(
+        context: context, // ignore: use_build_context_synchronously
+        builder: (context) => AlertDialog(
+          title: Text(e.toString()),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,7 +83,7 @@ class LoginPage extends StatelessWidget {
             // login button
             MyButton(
               text: "Login",
-              onTap: login,
+              onTap: () => login(context),
             ),
 
             const SizedBox(height: 25.0),
