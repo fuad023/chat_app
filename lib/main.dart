935176a4b2219c37d158dc507ef5249cedf05d82
fuad <nafisfuadisc@gmail.com>
2025-01-06 18:55:01 +0600

    Light Mode Theme Applied

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:chat_app/pages/login_page.dart';
+import 'package:chat_app/themes/light_mode.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: const LoginPage(),
+      theme: lightMode,
     );
   }
 }
